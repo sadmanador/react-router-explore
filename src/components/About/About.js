78d819cb464398448b1d408180ee945cc0af8512
sadmanador <84454116+sadmanador@users.noreply.github.com>
2022-11-14 17:07:47 +0600

    Link Header component is added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const About = () => {
     return (
         <div>
+            <Header></Header>
             <h4>This is About component</h4>
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
             <h5>This is Home component</h5>
         </div>
     );
