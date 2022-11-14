8350784cd9d25499bec9923f8b029e4b2c865dd0
sadmanador <84454116+sadmanador@users.noreply.github.com>
2022-11-14 17:00:03 +0600

    files and packages are added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+
+function App() {
+  const router = createBrowserRouter([
+    {path: '/', element:<div>This is default page</div>},
+    { path:'/home', element: <Home></Home>},
+    { path: '/about', element: <About></About>}
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h5>This is Home component</h5>
+        </div>
+    );
+};
+
+export default Home;
