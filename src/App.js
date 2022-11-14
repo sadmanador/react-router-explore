6d3e895b4ca19f23751055eeea473858e458124a
sadmanador <84454116+sadmanador@users.noreply.github.com>
2022-11-14 17:19:14 +0600

    Module 52-2 in done!

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Products from './components/Products/Products';
 
 function App() {
   const router = createBrowserRouter([
     {path: '/', element:<div>This is default page</div>},
     { path:'/home', element: <Home></Home>},
-    { path: '/about', element: <About></About>}
+    { path: '/about', element: <About></About>},
+    {path: '/products', element: <Products></Products>}
   ])
   return (
     <div className="App">
---
+++ src/components/Products/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Products = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h1>THis is product route</h1>
+        </div>
+    );
+};
+
+export default Products;
